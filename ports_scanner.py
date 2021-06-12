03bb68c260ba7cec1e9b28b71925d9ddef75a5f7
mhamedbelhachemi <mhamed79@live.fr>
2021-06-12 21:18:57 +0200

    ports scanner 2.0 | we can now scan multiple targets (ips)

--- ports_scanner.py
+++ ports_scanner.py
@@ -1,6 +1,19 @@
 import socket
 from IPy import IP
 
+#string colors
+OKBLUE = '\033[94m'
+ENDC = '\033[0m'
+BOLD = '\033[1m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+
+def scan(target):
+    converted_ip = checkIP(target)
+    print("\n"+ "[scanning target] "+ BOLD+OKBLUE+ str(target)+ENDC+ENDC)
+    for port in range(1,100):
+        scan_port(converted_ip,port)
+
 def checkIP(ip):
     try:
         IP(ip)
@@ -17,8 +30,10 @@ def scan_port(ipadress, port):
     except:
         print("[-] Port "+ str(port) +" is closed")
 
-ipadress = input("[+] Enter Target to scan :")
-converted_ip = checkIP(ipadress)
-
-for i in range(75,85):
-    scan_port(converted_ip,i)
+targets = input("[+] Enter Target(s) to scan ( for mutiples targets, split with ',' ): ")
+ 
+if ',' in targets:
+    for ipadress in targets.split(','):
+        scan(ipadress)
+else:
+    scan(targets)
