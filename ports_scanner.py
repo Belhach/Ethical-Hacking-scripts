836f27dcd7fbcd99994a52bf715db49b334841fd
mhamedbelhachemi <mhamed79@live.fr>
2021-06-13 03:12:38 +0200

    ports scanner 2.1 | protocol version getter added

--- ports_scanner.py
+++ ports_scanner.py
@@ -11,7 +11,7 @@ FAIL = '\033[91m'
 def scan(target):
     converted_ip = checkIP(target)
     print("\n"+ "[scanning target] "+ BOLD+OKBLUE+ str(target)+ENDC+ENDC)
-    for port in range(1,100):
+    for port in range(20,30):
         scan_port(converted_ip,port)
 
 def checkIP(ip):
@@ -21,14 +21,21 @@ def checkIP(ip):
     except ValueError :
         return socket.gethostbyname(ip)    
 
+def get_banner(s):
+    return s.recv(1024)
+
 def scan_port(ipadress, port):
     try:
         sock = socket.socket()
         sock.settimeout(0.5)
         sock.connect((ipadress, port))
-        print("[+] Port "+ str(port) +" is open")
+        try:
+            banner = get_banner(sock)
+            print("[+] Port "+ str(port) +" is"+OKBLUE+" open"+ENDC+" : "+str(banner.decode().strip('\n')))
+        except:
+            print("[+] Port "+ str(port) +" is"+OKBLUE+" open"+ENDC)   
     except:
-        print("[-] Port "+ str(port) +" is closed")
+        print("[-] Port "+ str(port) +" is"+FAIL+" closed"+ENDC)
 
 targets = input("[+] Enter Target(s) to scan ( for mutiples targets, split with ',' ): ")
  
